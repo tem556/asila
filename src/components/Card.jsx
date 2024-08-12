ded6b9f8edec8b713751c6861be2118a54e96b97
tem556 <tem556@github.com>
2024-08-12 18:06:27 +0300

    Split cols into two functions

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,50 +3,63 @@ import Book from '../assets/Book.png'
 import Medal from '../assets/Medal.png'
 import './Card.css'
 
+const FirstCol = () => (
+    <Col className="text-right order-2 order-md-1 col-pos">
+        <Row className="left-row-pos">
+            <h5 className="custom-card-font">اختر المستوى الأكاديمي:</h5>
+            <DropdownButton
+                id="dropdown-basic-button"
+                onSelect={() => console.log('selected dropdown')}
+                title="12 علمي"
+                className="dropdown-pos"
+                align="end"
+            >
+                <Dropdown.Item href="#/action-1">علمي 11</Dropdown.Item>
+            </DropdownButton>
+        </Row>
+        <Row className="bottom-left-row-pos">
+            <h5 className="custom-card-font">اختر الفصل الدراسي:</h5>
+            <DropdownButton id="dropdown-basic-button" title="الفصل الأول" className="dropdown-pos" align="end">
+                <Dropdown.Item href="#/action-1">الفصل الأول</Dropdown.Item>
+                <Dropdown.Item href="#/action-1">الفصل الثاني</Dropdown.Item>
+            </DropdownButton>
+        </Row>
+    </Col>
+)
+
+const SecondCol = () => (
+    <Col className="d-flex flex-column justify-content-center align-items-start text-right order-1 order-md-2">
+        <Row className="row-pos">
+            <Col className="d-flex">
+                <p className="custom-card-font">
+                    أفضل محتوى للتفوق و تحسين المستوى الأكاديمي لدى طلاب المدارس الحكومية في قطر
+                </p>
+            </Col>
+            <Col xs={1} className="d-flex justify-content-center img-col-pos">
+                <Image src={Medal} className="img-size-medal" />
+            </Col>
+        </Row>
+        <Row className="bottom-row-pos">
+            <Col className="d-flex">
+                <p className="custom-card-font">
+                    مئات الملازم في شتى المواضيع في مكان واحد, سهلة التحميل و الاستخدام
+                </p>
+            </Col>
+            <Col xs={1} className="d-flex justify-content-center bottom-img-col-pos">
+                <Image src={Book} className="img-size" />
+            </Col>
+        </Row>
+    </Col>
+)
+
+
 function InfoCard() {
     return (
         <div className="full-width-wrapper">
             <Container fluid>
                 <Row className="flex-column flex-md-row full-width-box below-navbar">
-                    <Col className="text-right order-2 order-md-1 col-pos">
-                        <Row className="left-row-pos">
-                            <h5 className="custom-card-font">اختر المستوى الأكاديمي:</h5>
-                            <DropdownButton id="dropdown-basic-button" title="12 علمي" className="dropdown-pos" align="end">
-                                <Dropdown.Item href="#/action-1">علمي 11</Dropdown.Item>
-                            </DropdownButton>
-                        </Row>
-                        <Row className="bottom-left-row-pos">
-                            <h5 className="custom-card-font">اختر الفصل الدراسي:</h5>
-                            <DropdownButton id="dropdown-basic-button" title="الفصل الأول" className="dropdown-pos" align="end">
-                                <Dropdown.Item href="#/action-1">الفصل الأول</Dropdown.Item>
-                                <Dropdown.Item href="#/action-1">الفصل الثاني</Dropdown.Item>
-                            </DropdownButton>
-                        </Row>
-                    </Col>
-
-
-                    <Col className="d-flex flex-column justify-content-center align-items-start text-right order-1 order-md-2">
-                        <Row className="row-pos">
-                            <Col className="d-flex">
-                                <p className="custom-card-font">
-                                    أفضل محتوى للتفوق و تحسين المستوى الأكاديمي لدى طلاب المدارس الحكومية في قطر
-                                </p>
-                            </Col>
-                            <Col xs={1} className="d-flex justify-content-center img-col-pos">
-                                <Image src={Medal} className="img-size-medal"/>
-                            </Col>
-                        </Row>
-                        <Row className="bottom-row-pos">
-                            <Col className="d-flex">
-                                <p className="custom-card-font">
-                                    مئات الملازم في شتى المواضيع في مكان واحد, سهلة التحميل و الاستخدام
-                                </p>
-                            </Col>
-                            <Col xs={1} className="d-flex justify-content-center bottom-img-col-pos">
-                                <Image src={Book} className="img-size"/>
-                            </Col>
-                        </Row>
-                    </Col>
+                    <FirstCol />
+                    <SecondCol />
                 </Row>
             </Container>
         </div>
